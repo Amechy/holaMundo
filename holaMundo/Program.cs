67719c102a45cfea8b37f84fbef78578d9069104
Alejandro Mechine <alejandromechinegamarra@gmail.com>
2017-04-04 11:04:24 +0200

    Commit segundo

--- holaMundo/Program.cs
+++ holaMundo/Program.cs
@@ -7,6 +7,7 @@ namespace holaMundo
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello Darkness my old friends");
+            Console.WriteLine("Pero que pasas");
 			Console.ReadLine();
 		}
 	}
